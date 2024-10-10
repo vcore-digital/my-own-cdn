08638ae8daccffc057b9392753315946fbfa45cd
Anton Vanyukov <a.vanyukov@vcore.ru>
2024-10-10 14:44:48 +1000

    Initial commit

---
+++ my-own-cdn.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * My Own CDN - CDN made simple for effortless performance
+ *
+ * @link              https://myowncdn.com
+ * @since             1.0.0
+ * @package           My_Own_CDN
+ *
+ * @wordpress-plugin
+ * Plugin Name:       My Own CDN
+ * Plugin URI:        https://myowncdn.com
+ * Description:       CDN made simple for effortless performance.
+ * Version:           1.0.0
+ * Author:            Anton Vanyukov
+ * Author URI:        https://vcore.au
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       my-own-cdn
+ * Domain Path:       /languages
+ * Network:           true
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Begins execution of the plugin.
+ *
+ * @since 1.0.0
+ */
+function my_own_cdn_init() {
+
+}
+my_own_cdn_init();
